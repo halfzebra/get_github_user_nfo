7eb591a88e1395daf97d2aff124cdc612b2b6a5b
Drew11 <drew.naumenko@mail.com>
2016-11-15 19:57:54 +0200

    add function

--- js/index.js
+++ js/index.js
@@ -11,41 +11,38 @@ $(document).ready(function () {
             var dataUser = JSON.parse(xhr.responseText);
             var out = "";
             for (var i in dataUser) {
-                out = out + '<br><b>' + dataUser[i] + '<b>'
+                out = out + '<br><b>' + i + ' :' + dataUser[i] + '<b>'
             }
             outputNfoUser.html(out);
         };
-
         var xhrRepos = new XMLHttpRequest();
         xhrRepos.open('GET', 'https://api.github.com/users/' + userName + '/repos', true);
         xhrRepos.send();
-
         xhrRepos.onload = function () {
             var dataUserRepos = JSON.parse(xhrRepos.responseText);
-            var out = "";
+            var outRepos = "";
             for (var k in dataUserRepos) {
-                out = out + '<br><b>' + dataUserRepos[k].name + '<b>';
-                console.log(dataUserRepos);
-                var xhrReposLang = new XMLHttpRequest();
-                xhrReposLang.open('GET', 'https://api.github.com/repos/' + userName + '/' + dataUserRepos[k].name + '/languages', true);
-                xhrReposLang.send();
-                xhrReposLang.onload = function () {
-                    var dataUserLang = JSON.parse(xhrReposLang.responseText);
-                    var outLang = "";
-                    for (var l in dataUserLang) {
-                        outLang = outLang + '<br><b>' + 'JavaScript:' + dataUserLang[l] + '<b>'
-
-                    }
-                    $("#javaScrtipt").html(outLang);
-
-
-            };
-            outputReposUser.html(out);
-
-
-
+                var userRepos = dataUserRepos[k].name;
+                outRepos = outRepos + '<br><b>' + k + ' :' + userRepos + '<b>';
+                outputReposUser.html(outRepos);
+                console.log(userRepos);
+                f(userRepos);
             }
         };
+
+        function f(userRepos) {
+            var xhrReposLang = new XMLHttpRequest();
+            xhrReposLang.open('GET', 'https://api.github.com/repos/' + userName + '/' + userRepos + '/languages', true);
+            xhrReposLang.send();
+            xhrReposLang.onload = function () {
+                var dataUserLang = JSON.parse(xhrReposLang.responseText);
+                var outLang = "";
+                for (var l in dataUserLang) {
+                    outLang = outLang + '<br><b>' + l + ':' + dataUserLang[l] + '</b>';
+                }
+                $("#javaScrtipt").html(outLang);
+            }
+        }
     })
 });
   
