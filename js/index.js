67a826e1e0b0d3ca12a9b86a853fd9f65a16b9d8
Drew11 <drew.naumenko@mail.com>
2016-12-06 12:48:29 +0200

    add function return array repos

--- js/index.js
+++ js/index.js
@@ -16,22 +16,35 @@ $(document).ready(function () {
                 }
             }
             outputNfoUser.html(out);
+
         };
         var xhrRepos = new XMLHttpRequest();
         xhrRepos.open('GET', 'https://api.github.com/users/' + userName + '/repos', true);
         xhrRepos.send();
-        xhrRepos.onload = function () {
+        
+        xhrRepos.onload=function () {
             var dataUserRepos = JSON.parse(xhrRepos.responseText);
             var outRepos = "";
+            var arrDataCharts= [];
 
             for (var k in dataUserRepos) {
+
+
+                arrDataCharts.push(dataUserRepos[k].name);
                 var userRepos = dataUserRepos[k].name;
-                console.log(userRepos);
                 outRepos = outRepos + '<br><b>' + k + ' :' + userRepos + '<b>';
                 outputReposUser.html(outRepos);
                 f(userRepos);
+
+
             }
+            function r() {
+                return console.log(arrDataCharts);
+            }r()
+
         };
+
+
 
         function f(userRepos) {
             var xhrReposLang = new XMLHttpRequest();
@@ -44,6 +57,50 @@ $(document).ready(function () {
                 }
             }
         }
-    })
+
+
+
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels:xhrRepos.r,
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+
+    });
+
+    
 });
   
